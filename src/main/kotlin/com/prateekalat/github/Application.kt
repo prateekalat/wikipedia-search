e2a26753dfca4be9ff2f0be295819947847dee6d
Prateek Alat <prateek.alat@gmail.com>
2018-08-21 22:56:13 +0530

    - Updated build.gradle files

--- src/main/kotlin/com/prateekalat/github/Application.kt
+++ src/main/kotlin/com/prateekalat/github/Application.kt
@@ -12,76 +12,73 @@ import com.google.code.externalsorting.ExternalSort.*
 import java.io.File
 import javax.xml.parsers.SAXParserFactory
 
-class Application {
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            val startTime = System.currentTimeMillis()
+//    companion object {
+//        @JvmStatic
+fun main(args: Array<String>) {
+    val startTime = System.currentTimeMillis()
 
-            var pageId = 1
+    var pageId = 1
 
-            val unsortedIndexFile = File("Z:/SearchEngine/out.txt").apply { if (exists()) delete() }
-            val writer = unsortedIndexFile.bufferedWriter()
+    val unsortedIndexFile = File("Z:/SearchEngine/out.txt").apply { if (exists()) delete() }
+    val writer = unsortedIndexFile.bufferedWriter()
 
-            val parserFactory = SAXParserFactory.newInstance()
-            val saxParser = parserFactory.newSAXParser()
-            val cleaner = Cleaner()
-            val tokenizer = Tokenizer()
-            val stemmer = Stemmer()
-            val indexGenerator = IndexGenerator()
+    val parserFactory = SAXParserFactory.newInstance()
+    val saxParser = parserFactory.newSAXParser()
+    val cleaner = Cleaner()
+    val tokenizer = Tokenizer()
+    val stemmer = Stemmer()
+    val indexGenerator = IndexGenerator()
 
-            val handler = PageHandler(object : PageCallback {
-                override fun process(page: Page) {
+    val handler = PageHandler(object : PageCallback {
+        override fun process(page: Page) {
 
-                    val textParser = TextParser(page.wikiText)
+            val textParser = TextParser(page.wikiText)
 
-                    val cleanedText = cleaner.clean(textParser.textBody)
-                    val cleanedCategories = cleaner.clean(textParser.categories)
-                    val cleanedTitle = cleaner.clean(page.title)
-                    val cleanedInfoBox = cleaner.clean(textParser.infoBox.dumpRaw() ?: "")
-                    val cleanedLinks = cleaner.clean(textParser.links)
+            val cleanedText = cleaner.clean(textParser.textBody)
+            val cleanedCategories = cleaner.clean(textParser.categories)
+            val cleanedTitle = cleaner.clean(page.title)
+            val cleanedInfoBox = cleaner.clean(textParser.infoBox.dumpRaw() ?: "")
+            val cleanedLinks = cleaner.clean(textParser.links)
 
-                    val textTokens = tokenizer.tokenize(cleanedText)
-                    val categoryTokens = tokenizer.tokenize(cleanedCategories)
-                    val titleTokens = tokenizer.tokenize(cleanedTitle)
-                    val infoBoxTokens = tokenizer.tokenize(cleanedInfoBox)
-                    val linkTokens = tokenizer.tokenize(cleanedLinks)
+            val textTokens = tokenizer.tokenize(cleanedText)
+            val categoryTokens = tokenizer.tokenize(cleanedCategories)
+            val titleTokens = tokenizer.tokenize(cleanedTitle)
+            val infoBoxTokens = tokenizer.tokenize(cleanedInfoBox)
+            val linkTokens = tokenizer.tokenize(cleanedLinks)
 
-                    val stemmedTextTokens = stemmer.stemTreeMap(textTokens)
-                    val stemmedCategoryTokens = stemmer.stemTreeMap(categoryTokens)
-                    val stemmedTitleTokens = stemmer.stemTreeMap(titleTokens)
-                    val stemmedInfoBoxTokens = stemmer.stemTreeMap(infoBoxTokens)
-                    val stemmedLinkTokens = stemmer.stemTreeMap(linkTokens)
+            val stemmedTextTokens = stemmer.stemTreeMap(textTokens)
+            val stemmedCategoryTokens = stemmer.stemTreeMap(categoryTokens)
+            val stemmedTitleTokens = stemmer.stemTreeMap(titleTokens)
+            val stemmedInfoBoxTokens = stemmer.stemTreeMap(infoBoxTokens)
+            val stemmedLinkTokens = stemmer.stemTreeMap(linkTokens)
 
-                    val index = indexGenerator.generateIndex(
-                            pageId,
-                            stemmedTextTokens,
-                            stemmedCategoryTokens,
-                            stemmedTitleTokens,
-                            stemmedInfoBoxTokens,
-                            stemmedLinkTokens
-                    )
+            val index = indexGenerator.generateIndex(
+                    pageId,
+                    stemmedTextTokens,
+                    stemmedCategoryTokens,
+                    stemmedTitleTokens,
+                    stemmedInfoBoxTokens,
+                    stemmedLinkTokens
+            )
 
-                    writer.append(index)
+            writer.append(index)
 
-                    pageId++
-                }
-            })
-            saxParser.parse(File("Z:/SearchEngine/wiki-search-small.xml"), handler)
-
-            val comparator: Comparator<String> = Comparator { o1, o2 ->
-                o1.split(":")[0].compareTo(o2.split(":")[0])
-            }
-
-            val listOfFiles = sortInBatch(unsortedIndexFile, comparator,
-                    File("Z:/SearchEngine/"))
-
-            mergeSortedFiles(listOfFiles, File("Z:/SearchEngine/index.txt"), comparator)
-
-            val currentTime = System.currentTimeMillis()
-            System.out.println("%d seconds".format((currentTime - startTime) / 1000))
-
-            writer.close()
+            pageId++
         }
+    })
+    saxParser.parse(File("Z:/SearchEngine/wiki-search-small.xml"), handler)
+
+    val comparator: Comparator<String> = Comparator { o1, o2 ->
+        o1.split(":")[0].compareTo(o2.split(":")[0])
     }
+
+    val listOfFiles = sortInBatch(unsortedIndexFile, comparator,
+            File("Z:/SearchEngine/"))
+
+    mergeSortedFiles(listOfFiles, File("Z:/SearchEngine/index.txt"), comparator)
+
+    val currentTime = System.currentTimeMillis()
+    System.out.println("%d seconds".format((currentTime - startTime) / 1000))
+
+    writer.close()
 }
